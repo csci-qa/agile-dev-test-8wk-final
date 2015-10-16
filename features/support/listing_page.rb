d20cfb9a40fc15714dd537eaeccbcf5ed790321b
Bennett Wenger <bwenger2@student.cscc.edu>
2015-10-15 21:54:19 -0400

    created _page.rb files

---
+++ features/support/listing_page.rb
@@ -0,0 +1,6 @@
+require 'page-object'
+
+class ListingPage
+  include PageObject
+
+  page_url "http://localhost:4567/create"
---
+++ features/support/rubular_page.rb
@@ -0,0 +1,6 @@
+require 'page-object'
+
+class RubularPage
+  include PageObject
+
+  page_url "http://localhost:4567/create"
