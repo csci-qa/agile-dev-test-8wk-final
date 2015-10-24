187a8c043dd60cdbfbfbbfee1b946c2ab6008a03
Corby Lane <clane8@student.cscc.edu>
2015-10-24 15:00:54 -0400

    added rubular feature and pending steps

---
+++ features/step_definitions/rubular_steps.rb
@@ -0,0 +1,20 @@
+Given /^I am on the Rubular home page$/ do 
+  Pending
+end
+
+When /^I enter the regex (".*)@(.*)\.(.*)"$/ do
+  Pending
+end
+
+And /^I enter jdenen@cscc.edu$/ do
+  Pending
+end
+
+Then /^the match result is jdenen@cscc.edu$/ do
+  Pending
+end
+
+And /^returns three match groups on new lines with index$/ do
+  Pending
+end
+
