c54e20f4ac62aaab74163dc74cfcc57e744a40c5
Tyler Coy <tcoy1@student.cscc.edu>
2015-11-10 19:49:44 -0500

    Course Listing Steps now Pending - Need Then Pendings

---
+++ course_listing/features/step_definitions/search_steps.rb
@@ -0,0 +1,16 @@
+When /^I search for CSCI 2994$/ do
+	pending "WIP"
+	# search for CSCI 2994 at cscc.edu
+	# check for multiple results
+	# search for CSCI 2994 at cscc.edu
+	# check for CSCI-2994 - CSCI Current Topics
+		# as the first result
+end
+
+When /^I look at the course CSCI 2994$/ do
+	pending "WIP"
+	# check the course listing for CSCI 2994
+	# check that the prerequistites are None
+	# check that the bookstore website can be
+		# opened to the required text book
+end
