4c96956992dc7e94255a94f8b9c2b8925e7af0bf
Tyler Coy <tcoy1@student.cscc.edu>
2015-11-14 17:46:50 -0500

    Steps Stubbed

--- course_listing/features/step_definitions/search_steps.rb
+++ course_listing/features/step_definitions/search_steps.rb
@@ -6,6 +6,14 @@ When /^I search for CSCI 2994$/ do
 	# check for CSCI-2994 - CSCI Current Topics
 		# as the first result
 end
+Then /^the search yields more than on result$/ do
+	pending "WIP"
+	# search returns multiple results
+end
+Then /^the first result is "CSCI-2994 - CSCI Current Topics"$/ do
+	pending "WIP"
+	# the first result is "CSCI-2994 - CSCI Current Topics"
+end
 
 When /^I look at the course CSCI 2994$/ do
 	pending "WIP"
@@ -13,4 +21,12 @@ When /^I look at the course CSCI 2994$/ do
 	# check that the prerequistites are None
 	# check that the bookstore website can be
 		# opened to the required text book
+end
+Then /^the course listing has None for its prerequisites$/ do
+	pending "WIP"
+	# the course listing lists none for prerequistites
+end
+Then /^I can open the bookstore site to the required text book$/ do
+	pending "WIP"
+	# the requirred textbook can be viewed at the bookstore website
 end
