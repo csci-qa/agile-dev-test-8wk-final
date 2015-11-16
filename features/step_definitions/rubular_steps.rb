f106aa4c4ad3cfbb3c5e83448f05c94c057e88d7
Scott Felske <sbfelske@gmail.com>
2015-11-15 19:32:24 -0500

    update schedule and rubular

---
+++ features/step_definitions/rubular_steps.rb
@@ -0,0 +1,33 @@
+When /^I enter a regexp of "(.*)@(.*)\.(.*)" and a test string of "jdenen@cscc.edu"$/ do
+#step def here
+end
+
+Then /^I should return a match result of "jdenen@cscc.edu"$/ do	
+#step def here
+end
+
+When /^I enter a regexp of "(.*)@(.*)\.(.*)" and a test string of "jdenen@cscc.edu"$/ do
+#step def here
+end
+
+Then /^it should return three match groups$/ do
+#step def here
+end
+
+When /^I enter a regexp of "(.*)@(.*)\.(.*)" and a test string of "jdenen@cscc.edu"$/ do
+#step def here
+end
+
+Then /^each match group should be formatted onto a new line$/ do
+#step def here
+end
+
+When /^I enter a regexp of "(.*)@(.*)\.(.*)" and a test string of "jdenen@cscc.edu"$/ do
+#step def here
+end
+
+Then /^each match group should be preceded by an index number$/ do
+#step def here
+end
+
+    
---
+++ features/step_definitions/schedule_steps.rb
@@ -0,0 +1,35 @@
+When /^I search for CSCI 2994 from the CSCC homepage$/ do
+# step definitions go here
+end
+
+Then/^there are multiple listings$/ do
+# step def here
+end
+
+When /^I check the first search result$/ do
+# step def here
+end
+
+Then /^it is listed as CSCI 2994 - Current topics$/ do
+#step def here
+end
+
+When /^I look for prerequisites$/ do
+#step def here
+end
+
+Then /^I find that it lists None$/ do
+#step def here
+end
+
+When /^I look under the Textbooks heading$/ do
+#step def here
+end
+
+Then /^I find a link to the bookstore$/ do
+#step def herer
+end
+
+
+	
+	
