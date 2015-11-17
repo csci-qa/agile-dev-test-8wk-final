dd8020519983f3e4c04fcc11fc5b34262be7752e
Ben Tubbs <btubbs3@student.cscc.edu>
2015-11-17 18:49:54 -0500

    revision1

---
+++ features/step_definitions/course_steps.rb
@@ -0,0 +1,25 @@
+
+@@ -0,0 +1,23 @@
+When /^I search for "CSCI 2994" at CSCCs homepage$/ do
+end
+
+Then /^I get more than one result$/ do
+end
+
+When /^I execute the initial search$/ do
+end
+
+Then /^I get "CSCI-2994 - CSCI Current Topics" as the first result$/ do
+end
+
+When /^I view the course listing$/ do
+end
+
+Then /^I get no prerequisites for the course$/ do
+end
+
+When /^I look under textbooks in the course listing$/ do
+end
+
+Then /^there is a link to the bookstore under "Textbooks"$/ do
+end
---
+++ features/step_definitions/rubular_steps.rb
@@ -0,0 +1,31 @@
+
+@@ -0,0 +1,29 @@
+When /^I enter the regexp of "(.\*)@(.\*)\\.(.*)" and a test string of "jdenen@cscc.edu"$/ do
+	
+end
+
+Then /^I I get a match result of "jdenen@cscc.edu"$/ do
+
+end
+
+When /^I enter the regexp and "jdenen@cscc.edu"$/ do
+
+end
+
+Then /^I get three groups jdenen, cscc, edu$/ do
+
+end
+
+When /^I get the match results in three groups$/ do
+
+end
+
+Then /^they should be formatted onto a new line$/ do
+
+end
+
+When /^I get the match results from rubular$/ do
+end
+
+Then /^they should be preceaded by an index number$/ do
+end
