5f9297e3c93d9b583042e5964f906783519da52d
Ken Fry <kfry@student.cscc.edu>
2015-11-17 18:11:00 -0500

    new adds

--- features/step_definitions/course_steps.rb
+++ features/step_definitions/course_steps.rb
@@ -1,23 +1,31 @@
 When /^I search for "CSCI 2994" at CSCC's homepage$/ do
+	"pending"	
 end
 
 Then /^I get more than one result$/ do
+	"pending"
 end
 
 When /^I execute the initial search$/ do
+	"pending"
 end
 
 Then /^I get "CSCI-2994 - CSCI Current Topics" as the first result$/ do
+	"pending"
 end
 
 When /^I view the course listing$/ do
+	"pending"
 end
 
 Then /^I get no prerequisites for the course$/ do
+	"pending"
 end
 
 When /^I look under textbooks in the course listing$/ do
+	"pending"
 end
 
 Then /^there is a link to the bookstore under "Textbooks"$/ do
+	"pending"
 end
--- features/step_definitions/rubular_steps.rb
+++ features/step_definitions/rubular_steps.rb
@@ -1,29 +1,31 @@
 When /^I enter the regexp of "(.\*)@(.\*)\\.(.*)" and a test string of "fry975@gmail.com"$/ do
-	
+	"pending"
 end
 
 Then /^I I get a match result of "fry975@gmail.com"$/ do
-
+	"pending"
 end
 
 When /^I enter the regexp and "fry975@gmail.com"$/ do
-
+	"pending"
 end
 
 Then /^I get three groups fry975, gmail, com$/ do
-
+	"pending"
 end
 
 When /^I get the match results in three groups$/ do
-
+	"pending"
 end
 
 Then /^they should be formatted onto a new line$/ do
-
+	"pending"
 end
 
 When /^I get the match results from rubular$/ do
+	"pending"
 end
 
 Then /^they should be preceaded by an index number$/ do
+	"pending"
 end
