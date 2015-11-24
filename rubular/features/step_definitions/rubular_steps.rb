35452c50c7b915c31e14bbf7783e2a0a2447790d
tcoy1 <tcoy1@student.cscc.edu>
2015-11-24 16:53:28 -0500

    CSCC: Done; Rubular: Done

--- rubular/features/step_definitions/rubular_steps.rb
+++ rubular/features/step_definitions/rubular_steps.rb
@@ -1,45 +1,41 @@
 require 'watir-webdriver'
 
+# Hooks
 Before do
-#	puts "%%%%%%%%%%%%%%before%%%%%%%%%%"
 	@browser = Watir::Browser.new
 	@browser.goto 'www.rubular.com'
-	search_field = @browser.text_field(id: 'regex')
-	search_field.set('(.*)@(.*)\.(.*)')
-	test_field = @browser.textarea(id: 'test')
-	test_field.set('jden@cscc.edu')
+	@search_field = @browser.text_field(id: 'regex')
+	@search_field.set('(.*)@(.*)\.(.*)')
+	@test_field = @browser.textarea(id: 'test')
+	@test_field.set('jden@cscc.edu')
 end
 After do
-#	puts "$$$$$$$$$$$$$$$$$$$$$$after%%%%%%%%%%%%%%%"
 	@browser.close
 end
 
+# When I enter the regexp (.*)@(.*)\.(.*)
 When /^I enter the regexp \(\.\*\)@\(\.\*\)\\\.\(\.\*\) and test string jden@cscc\.edu$/ do
+  @test_field.send_keys(:enter)
+  sleep 2
 end
 Then /^it returns a match result of jden@cscc.edu$/ do
-#	puts "!!!!!!!!!!!!HERE!!!!!!!!!!!!!"
-	match = @browser.div(:id, "match_string").text
-#	puts "****************"
-#	puts match
-#	puts "****************"
-	expect(match)to eq "jden@cscc.edu"
+	match = @browser.div(id: 'match_string').span.span.text
+	expect(match).to eq("jden@cscc.edu")
 end
 Then /^it returns three match groups which are jdenen, cscc, and edu$/ do
-	groupOne = @browser.div(:id, "match_captures").table[1][2].text
-	groupTwo = @browser.div(:id, "match_captures").table[2][2].text
-	groupThree = @browser.div(:id, "match_captures").table[3][2].text
+	groupOne = @browser.div(:id, "match_captures").table[0][1].text
+	groupTwo = @browser.div(:id, "match_captures").table[1][1].text
+	groupThree = @browser.div(:id, "match_captures").table[2][1].text
 	expect(groupOne).to eq("jden")
 	expect(groupTwo).to eq("cscc")
 	expect(groupThree).to eq("edu")
-	#rubular returns three match fields: jdenen, cscc, and edu
 end
 Then /^the match groups are each formatted on a new line and preceded by a number$/ do
-	groupOne = @browser.div(:id, "match_captures").table[1][1].text
-	groupTwo = @browser.div(:id, "match_captures").table[2][1].text
-	groupThree = @browser.div(:id, "match_captures").table[3][1].text
-	expect(groupOne.to eq("1.")
+	groupOne = @browser.div(:id, "match_captures").table[0][0].text
+	groupTwo = @browser.div(:id, "match_captures").table[1][0].text
+	groupThree = @browser.div(:id, "match_captures").table[2][0].text
+	expect(groupOne).to eq("1.")
 	expect(groupTwo).to eq("2.")
 	expect(groupThree).to eq("3.")
-	# the three match groups are formatted on new line
-		# and preceded by number
+	# because each is in a new row of the table, they are each on a new line
 end
