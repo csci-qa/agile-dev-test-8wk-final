c5235c28a5b5c06975ed577542f6bbc135832d34
Ken Fry <kfry@student.cscc.edu>
2015-12-08 16:50:14 -0500

    done

--- features/step_definitions/rubular_steps.rb
+++ features/step_definitions/rubular_steps.rb
@@ -28,15 +28,19 @@ When /^I get the match results from rubular$/ do
 	expression_field = @browser.text_field(id: 'regex')
 	expression_field.set ('(.*)@(.*)\.(.*)')
 	expression_field.send_keys(:enter)
+	sleep 4
 	test_field = @browser.text_field(id: 'test')
 	test_field.set ('jdenen@cscc.edu')
 	test_field.send_keys(:enter)
+	sleep 4
 	
 end
 
 Then /^they should be preceded by an index number$/ do
-	result = @browser.div(class: 'match_captures')
-	expect(result).to eq "1. jdenen", "2. cscc", "3. edu"
+	result = @browser.div(id: 'match_captures')
+	expect(result.trs[0].text).to eq "1. jdenen"
+	expect(result.trs[1].text).to eq "2. cscc"
+	expect(result.trs[2].text).to eq "3. edu"
 	
 end
 
